87fb1823faec79f24edc4784aee2417cfafef1f8
Nicolas <nicolasfau.46@gmail.com>
2019-04-22 13:55:25 +0200

    v0.2 avec liste series et saisons dynamiques

---
+++ ajoutepisode.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+    <label for="choix_serie">Nom Série </label>
+    <form action="formajoutsaison.php" method="post">
+            <?
+            $n="test";
+            $u="test";
+            $p="123456789";
+            $connect=pg_connect("host=localhost port=5432 dbname=$n user=$u password=$p");
+            $queryNomserie="Select * from serie";
+            $resulatNomListe=pg_exec($connect, $queryNomserie);
+            //datalist dynamique
+            echo '<input  list="listeSerie" type="text" name="listeSerie">';
+            echo '<datalist id="listeSerie">';
+            while ($data =pg_fetch_array($resulatNomListe)) {
+              // on affiche les résultats
+              echo '<option value='.$data['nomserie'].'>';
+            }
+            echo  '</datalist>';
+          ?>
+          <label for="choix_saison">Saison </label>
+          <?
+          $n="test";
+          $u="test";
+          $p="123456789";
+          $connect=pg_connect("host=localhost port=5432 dbname=$n user=$u password=$p");
+          $queryNomserie="Select * from Serie,Saison WHERE Serie.NomSerie=Saison.idSaison";
+          $resulatNomListe=pg_exec($connect, $queryNomserie);
+          //datalist dynamique
+          echo '<input  list="listeSaison" type="text" name="listeSaison">';
+          echo '<datalist id="listeSaison">';
+          while ($data =pg_fetch_array($resulatNomListe)) {
+          	// on affiche les résultats
+          	echo '<option value='.$data['numeroSaison'].'>';
+          }
+          echo  '</datalist>';
+        ?>
+	       <p>Nom Episode <input type="text" name="nom"></p>
+	       <p>Numéro Episode<input type="number" id="tentacles" name="num" min="1" max="100"></p>
+         <p>Durée épisode<input type="text" name="duree"></p>
+         <p><a href="ajoutepisode.php"><input type="submit" value="Ajouter"></a></p>
+         <p><a href="admin.php"><input type="submit" value="Terminer"></a></p>
+
+    </form>
