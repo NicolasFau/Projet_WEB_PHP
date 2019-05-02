0dd54fd99cd0ff5a66409fd733e0bcb7b3aa06d7
Nicolas <nicolasfau.46@gmail.com>
2019-05-02 21:51:19 +0200

    final pret pour fusion Mais Bug!

---
+++ admin.php
@@ -0,0 +1,17 @@
+<?php require("header.php");
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+      <p><a href="ajoutserie.php"><input type="submit" name="1" value="Ajouter Serie"></a></p>
+      <p><a href="ajoutsaison.php"><input type="submit" name="1" value="Ajouter Saison"></a></p>
+      <p><a href="ajoutrealisateur.php"><input type="submit" name="1" value="Ajouter Réalisateur"></a></p>
+      <p><a href="ajoutprix.php"><input type="submit" name="1" value="Ajouter Prix"></a></p>
+      <p><a href="ajoutacteur.php"><input type="submit" name="1" value="Ajouter Acteur"></a></p>
+</body>
