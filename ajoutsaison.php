a879d4bfab4871f099a428ec8b0d61925970f19a
Nicolas <nicolasfau.46@gmail.com>
2019-05-03 10:30:38 +0200

    Ajout date jquery ajoutsaison

--- ajoutsaison.php
+++ ajoutsaison.php
@@ -6,7 +6,7 @@
     <head>
         <title>PHP</title>
         <meta charset="utf-8" />
-    </head>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="/resources/demos/style.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -14,7 +14,9 @@
     $( function() {
       $( "#datepicker" ).datepicker();
     } );
+
     </script>
+      </head>
     <body>
     <form action="formajoutsaison.php" method="post">
 	       <p>Date Sortie <input type="text" name="date" id="datepicker"></p>
