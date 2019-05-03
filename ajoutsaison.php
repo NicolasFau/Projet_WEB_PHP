c2dad4d1c8369de7c350b9b690f6d383ea581e7d
Nicolas <nicolasfau.46@gmail.com>
2019-05-03 10:27:47 +0200

    Ajout date jquery ajoutsaison

--- ajoutsaison.php
+++ ajoutsaison.php
@@ -7,9 +7,17 @@
         <title>PHP</title>
         <meta charset="utf-8" />
     </head>
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+    $( function() {
+      $( "#datepicker" ).datepicker();
+    } );
+    </script>
     <body>
     <form action="formajoutsaison.php" method="post">
-	       <p>Date Sortie <input type="date" name="date"></p>
+	       <p>Date Sortie <input type="text" name="date" id="datepicker"></p>
 	       <p>Numéro Saison<input type="number" id="tentacles" name="num" min="1" max="100"></p>
 
   <label for="choix_serie">Nom Série </label>
