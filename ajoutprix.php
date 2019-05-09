241a19bb469acb189828789aab44d0e90e063c79
Nicolas <nicolasfau.46@gmail.com>
2019-05-09 17:02:13 +0200

    Ajout tag

--- ajoutprix.php
+++ ajoutprix.php
@@ -1,36 +1,24 @@
-
-<?php require("header.php");
-
- ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>PHP</title>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script>
-  $( function() {
-    $( "#datepicker" ).datepicker();
-  } );
-  </script>
-
     </head>
     <body>
 
     <form action="formPrix.php" method="post">
 	       <p>Nom <input type="text" name="nom"></p>
 	       <p>Ville<input type="text" name="ville"></p>
-         <p>Date<input type="text" name="date" id="datepicker"></p>
-         <p><a href="ajoutprix.php"><input type="submit" value="Ajouter"></a></p>
+         <p>Date<input type="date" name="date"></p>
+         <p><a href="ajoutprix.php"<input type="submit" value="Ajouter"></a></p>
     </form>
     <form action="ajoutprixserie.php" method="post">
       <label for="choix_serie">Nom Série </label>
       <?
-      require("connexion.php");
-      $connect=$linkpdo;
+      $n="test";
+      $u="test";
+      $p="123456789";
+      $connect=pg_connect("host=localhost port=5432 dbname=$n user=$u password=$p");
       $queryNomserie="Select * from serie";
       $resulatNomListe=pg_exec($connect, $queryNomserie);
       //datalist dynamique
@@ -38,15 +26,17 @@
       echo '<datalist id="listeSerie">';
       while ($data =pg_fetch_array($resulatNomListe)) {
       	// on affiche les résultats
-      	echo '<option value="'.$data['nomserie'].'">';
+      	echo '<option value='.$data['nomserie'].'>';
       }
       echo  '</datalist>';
     ?>
 
   <label for="choix_prix">Prix </label>
   <?
-  require("connexion.php");
-  $connect=$linkpdo;
+  $n="test";
+  $u="test";
+  $p="123456789";
+  $connect=pg_connect("host=localhost port=5432 dbname=$n user=$u password=$p");
   $queryprix="Select * from prixdecerne";
   $resulatprix=pg_exec($connect, $queryprix);
   //datalist dynamique
@@ -54,30 +44,9 @@
   echo '<datalist id="listePrix">';
   while ($data =pg_fetch_array($resulatprix)) {
     // on affiche les résultats
-    echo '<option value="'.$data['nomprix'].'">';
+    echo '<option value='.$data['nomPrix'].'>';
   }
   echo  '</datalist>';
 ?>
     <p><a href="ajoutprix.php"><input type="submit" value="Ajouter"></a></p>
     </form>
-    <h2>Prix dans la base</h2>
-    <?
-    require("connexion.php");
-    $connect=$linkpdo;
-    $queryNomserie="Select * from prixdecerne";
-    $resulatNomListe=pg_exec($connect, $queryNomserie);
-    //datalist dynamique
-    echo '<table>';
-    echo "<tr>
-       <th>Nom</th>
-       <th>Ville</th>
-   </tr>";
-    while ($data =pg_fetch_array($resulatNomListe)) {
-      // on affiche les résultats
-      echo "<tr>";
-      echo '<td>'.$data['nomprix'].'</td>';
-      echo '<td>'.$data['villeprix'].'</td>';
-      echo "</tr>";
-    }
-    echo  '</table>';
-    ?>
--- ajoutsaison.php
+++ ajoutsaison.php
@@ -1,32 +1,20 @@
-<?php require("header.php");
-
- ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>PHP</title>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script>
-    $( function() {
-      $( "#datepicker" ).datepicker();
-    } );
-
-    </script>
-      </head>
+    </head>
     <body>
     <form action="formajoutsaison.php" method="post">
-	       <p>Date Sortie <input type="text" name="date" id="datepicker"></p>
+	       <p>Date Sortie <input type="date" name="date"></p>
 	       <p>Numéro Saison<input type="number" id="tentacles" name="num" min="1" max="100"></p>
 
   <label for="choix_serie">Nom Série </label>
 
-  <?
+  <?php
   require("connexion.php");
   $connect=$linkpdo;
+  
   $queryNomserie="Select * from serie";
   $resulatNomListe=pg_exec($connect, $queryNomserie);
   //datalist dynamique
@@ -34,8 +22,7 @@
   echo '<datalist id="listeSerie">';
   while ($data =pg_fetch_array($resulatNomListe)) {
   	// on affiche les résultats
-    echo $date['nomserie']."</br>";
-  	echo '<option value="'.$data['nomserie'].'">';
+  	echo '<option value='.$data['nomserie'].'>';
   }
   echo  '</datalist>';
 ?>
