475b4b8a496a7e2193d93d26b67d5785c406a98f
Nicolas <nicolasfau.46@gmail.com>
2019-05-13 00:17:37 +0200

    admin.php en cours de codage

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
-<?php require("header.php");
-
+<?php
+include 'head.php';
+include 'header.php';
  ?>
 
 <!DOCTYPE html>
@@ -14,4 +15,22 @@
       <p><a href="ajoutrealisateur.php"><input type="submit" name="1" value="Ajouter Réalisateur"></a></p>
       <p><a href="ajoutprix.php"><input type="submit" name="1" value="Ajouter Prix"></a></p>
       <p><a href="ajoutacteur.php"><input type="submit" name="1" value="Ajouter Acteur"></a></p>
+      <p><a href="supprimerUtilisateur.php"><input type="submit" name="1" value="Supprimer Utilisateur"></a></p>
+      <?php
+        require("connexion.php");
+        $querycritique="Select * from critique where estsignalee=True";
+        $querymotif="Select motifsignalement from signalement where idcritique='$data['idcritique']'";
+        $result=pg_query($linkpdo,$query);
+        echo "<form action=\"supressionCritique.php\" methode\"post\">";
+        while ($date = pg_fetch_array($result)) {
+          echo "<input type=\"checkbox\" name=\"supr[]\" value=\"".$data['codeutilisateurcritiquant']."\">".$data['aviscritique'];
+          $querymotif="Select motifsignalement from signalement where idcritique='$data['idcritique']'";
+          $result=pg_query($linkpdo,$querymotif);
+          $resulttab=pg_fetch_array($result);
+          echo "$resulttab['motifsignalement']";
+        }
+        echo "</form>";
+        echo "<p><input type=\"submit\" value=\"Supprimer\"></p>";
+       ?>
+
 </body>
