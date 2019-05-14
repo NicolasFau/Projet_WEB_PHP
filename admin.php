7ce3d2fadddbb875164b4bdc60bc8c5e9a15dd60
VincentVILLEMUR <villemurv@gmail.com>
2019-05-14 11:33:46 +0200

    UPDATE 14/05

--- admin.php
+++ admin.php
@@ -16,25 +16,21 @@ include 'header.php';
 <p><a href="ajoutprix.php"><input type="submit" name="1" value="Ajouter Prix"></a></p>
 <p><a href="ajoutacteur.php"><input type="submit" name="1" value="Ajouter Acteur"></a></p>
 <p><a href="supprimerUtilisateur.php"><input type="submit" name="1" value="Supprimer Utilisateur"></a></p>
-<h3>Critiques Signalées</h3>
 <?php
-require("connexion.php");
-$querycritique="Select * from critique where estsignalee='True'";
-$result=pg_query($linkpdo,$querycritique);
-echo "<form action=\"suppressionCritique.php\" method=\"post\">";
-while ($data = pg_fetch_array($result)) {
-    echo $data['codeutilisateurcritiquant'];
-    echo $data['aviscritique']."<input type=\"checkbox\" name=\"suppr[]\" value=\"".$data['idcritique']."\">";
-    $querymotif="Select motifsignalement from signalement where idcritique=".$data['idcritique'];
-    $result=pg_query($linkpdo,$querymotif);
-    $resulttab=pg_fetch_array($result);
-    echo "</br>Motif:</br>";
-    echo $resulttab['motifsignalement'];
-}
-echo "<div><input type=\"radio\" id=\"dewey\" name=\"choix\" value=\"Laisser\"><label for=\"dewey\">Laisser</label></div>";
-echo "<div><input type=\"radio\" id=\"dewey\" name=\"choix\" value=\"Supprimer\"><label for=\"dewey\">Supprimer</label></div>";
-echo "<p><input type=\"submit\" value=\"Valider\"></p>";
-echo "</form>";
-?>
+        require("connexion.php");
+        $querycritique="Select * from critique where estsignalee=True";
+        $querymotif="Select motifsignalement from signalement where idcritique='$data['idcritique']'";
+        $result=pg_query($linkpdo,$query);
+        echo "<form action=\"supressionCritique.php\" methode\"post\">";
+        while ($date = pg_fetch_array($result)) {
+          echo "<input type=\"checkbox\" name=\"supr[]\" value=\"".$data['codeutilisateurcritiquant']."\">".$data['aviscritique'];
+          $querymotif="Select motifsignalement from signalement where idcritique='$data['idcritique']'";
+          $result=pg_query($linkpdo,$querymotif);
+          $resulttab=pg_fetch_array($result);
+          echo "$resulttab['motifsignalement']";
+        }
+        echo "</form>";
+        echo "<p><input type=\"submit\" value=\"Supprimer\"></p>";
+       ?>
 
 </body>
