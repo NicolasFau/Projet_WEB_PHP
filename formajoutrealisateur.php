f7e2954b7a729a9aee0ed1e1b91f4849ff3ab9eb
NicolasFau <nicolasfau.46@gmail.com>
2019-05-16 15:00:31 +0200

    Ajout control réalisateur

--- formajoutrealisateur.php
+++ formajoutrealisateur.php
@@ -5,19 +5,31 @@ include 'header.php';
 $nom=$_POST['nom'];
 $prenom=$_POST['prenom'];
 $date=$_POST['date'];
-//Connexion bdd
-$connect=$linkpdo;
+$querycontrol="SELECT * FROM realisateur";
+$result=pg_query($linkpdo,$querycontrol);
+
+while ($tab=pg_fetch_array($result)) {
+      if($tab['nomrealisateur']==$nom && $tab['prenomrealisateur']==$prenom){
+        $exit=1;
+      }
+
+}
+
+if($exit!=1){
 //Requete
 $query="INSERT INTO realisateur(nomrealisateur, prenomrealisateur, ddnrealisateur)
         VALUES('$nom','$prenom','$date')";
 //Insertion dans la base
-$queryinsert=pg_query($connect,$query);
+$queryinsert=pg_query($linkpdo,$query);
 //Controle de l'insertion
 
 if ($queryinsert) {
   header("Location: ajoutrealisateur.php");
 } else {
-    echo "Echec";
+    echo "Location: erreur.php";
 }
-
+}
+else{
+    echo "Le réalisateur existe déjà";
+}
  ?>
