d46099ec6ea7382dd0cc0fe46e0f3a505383f7d3
NicolasFau <nicolasfau.46@gmail.com>
2019-05-16 16:48:56 +0200

    Ajout controle sur ajout des saisons

--- formajoutsaison.php
+++ formajoutsaison.php
@@ -6,25 +6,33 @@ include 'header.php';
 $date=$_POST['date'];
 $numeroSaison=$_POST['num'];
 $listeSerie=$_POST['listeSerie'];
-/*Debug
-echo $nomSerie;
-echo $numeroSaison;
-echo $listeSerie;
-//Connection à la base de donnée
-echo $date;*/
 
 
-$connect=$linkpdo;
+$querycontrol="SELECT * FROM saison WHERE nomserie='$listeSerie'";
+$result=pg_query($linkpdo,$querycontrol);
 
+while ($tab=pg_fetch_array($result)) {
+        //echo $tab['numérosaison'];
+        if($tab['numérosaison']==$numeroSaison){
+        $exit=1;
+      }
+
+}
+if($exit!=1){
 //Insertion du numéro de la Saison
-$queryinsert="INSERT INTO saison(numéroSaison, dateparutionsaison, nomserie) VALUES('$numeroSaison','$date','$listeSerie')";
-$queryNomserie=pg_query($connect,$queryinsert);
+$queryinsert="INSERT INTO saison(numérosaison, dateparutionsaison, nomserie) VALUES('$numeroSaison','$date','$listeSerie')";
+$queryNomserie=pg_query($linkpdo,$queryinsert);
 
 if ($queryNomserie) {
     echo "Succès";
     header("Location: ajoutepisode.php");
 } else {
-    echo "Echec";
+    echo "Location: erreur.php";
+}
+
+}
+else{
+    echo "La saison existe déjà";
 }
 
 
