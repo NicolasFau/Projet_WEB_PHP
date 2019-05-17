06ce3bdfe8288ef6996bca755b77e280ceb48e1e
NicolasFau <nicolasfau.46@gmail.com>
2019-05-17 10:53:02 +0200

    ajout control saisie episode

--- formajoutepisode.php
+++ formajoutepisode.php
@@ -9,19 +9,39 @@ $saison=$_POST['listeSaison'];
 $nomepisode=$_POST['nom'];
 $numeroepisode=$_POST['num'];
 $duree=$_POST['duree'];
-//Connexion bdd
-$connect=$linkpdo;
+
+$querysaison="SELECT * FROM saison WHERE  numérosaison='$saison'";
+$resultid=pg_query($linkpdo,$querysaison);
+$tabid=pg_fetch_array($resultid);
+$idsaison=$tabid['idsaison'];
+
+//Controle
+$querycontrol="SELECT * FROM episode WHERE idsaison='$idsaison'";
+$result=pg_query($linkpdo,$querycontrol);
+
+while ($tab=pg_fetch_array($result)) {
+       
+        if($tab['numeroepisode']==$numeroepisode){
+        $exit=1;
+      }
+
+}
+if($exit!=1){
 //Requete d'insertion à affinnée selon correction model
-$query="INSERT INTO episode(nomepisode,numeroepisode,duréeeepisode, idsaison)
-          VALUES('$nomEpisode','$numeroepisode','$duree','$saison')";
-$insert=pg_query($connect,$query);
+
+$query="INSERT INTO episode(nomepisode,numeroepisode,duréeepisode, idsaison)
+          VALUES('$nomepisode','$numeroepisode','$duree','$idsaison')";
+$insert=pg_query($linkpdo,$query);
 //Controle d'insertion
 
 if ($insert) {
     echo "Succès.\n";
-    header("Location: ajouteepisode.php");
+    header("Location: ajoutepisode.php");
 } else {
-    echo "Echec\n";
+    echo header("Location: erreur.php");
 }
-
+}
+else{
+	echo "L episode existe deja";
+}
  ?>
