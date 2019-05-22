043923d1eb3522ec4bc238295fa9cc6cee2c08ea
NicolasFau <nicolasfau.46@gmail.com>
2019-05-22 19:36:45 +0200

    bug fix

--- ajoutprix.php
+++ ajoutprix.php
@@ -35,7 +35,7 @@ include 'head.php';
       $queryNomserie="Select * from serie";
       $resulatNomListe=pg_exec($linkpdo, $queryNomserie);
       //datalist dynamique
-      echo '<select id="listeSerie">';
+      echo '<select name="1"  id="listeSerie">';
 
       while ($data =pg_fetch_array($resulatNomListe)) {
       	// on affiche les résultats
@@ -51,7 +51,7 @@ include 'head.php';
   $queryprix="Select * from prixdecerne";
   $resulatprix=pg_exec($linkpdo, $queryprix);
   //datalist dynamique
-  echo '<select id="listePrix">';
+  echo '<select name="2"  id="listePrix">';
 ;
   while ($data =pg_fetch_array($resulatprix)) {
     // on affiche les résultats
